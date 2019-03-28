d523994809d9f3dfbeb9334df1fdd63e01b35c3d
Akilia <pm@akilia.net>
2019-03-28 08:57:04 +0100

    Compatibilité avec le plugin RANG : suppression de la fonction motsdf_test_rang_actif() car la balise #RANG_LISTE_OBJETS fournie par le plugin Rang fait très bien le job.

--- motsdf_fonctions.php
+++ motsdf_fonctions.php
@@ -14,27 +14,3 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 include_spip('inc/motsdf_api');
-
-
-/**
- * Vérifier si le plugin rang est actif et si oui, qu'il a ete active sur les mots cles
- * Permet par exemple d'utiliser ou non le critere {par rang}
- *
- * @param int $id_mot
- * 
- * @param int $id_objet
- * 
- * @return bool
- *     true si Rang a été active sur les mots-cles
-**/
-function motsdf_test_rang_actif($objet) {
-
-	$table = table_objet_sql($objet);
-	$table_sql = lister_tables_objets_sql($table);
-	if (isset($table_sql['field']['rang'])) {
-		return true;
-	} 
-	else {
-		return false;
-	}
-}
