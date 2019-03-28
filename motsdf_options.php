5c037708be5dd7353dada3e05799e07dd7c0e794
Akilia <pm@akilia.net>
2019-03-28 10:39:51 +0100

    hop

--- motsdf_options.php
+++ motsdf_options.php
@@ -12,8 +12,3 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
-$opt = array('rubriques_on' => array(0=>2, 1=>14));
-debug($opt);
-$blah = autoriser('dansrubrique', 'groupemots', '14', '1', $opt);
-debug($blah);
