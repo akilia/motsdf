8dc9179efc5d739b502f5f8dca0fa02a377151e6
Akilia <pm@akilia.net>
2020-06-03 12:10:26 +0200

    PHPDoc

--- motsdf_fonctions.php
+++ motsdf_fonctions.php
@@ -16,6 +16,18 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 include_spip('inc/motsdf_api');
 
 
+/**
+ * Permet de retrouver rapidement l'id_mot affecté à un objet
+ *
+ * @example
+ *  #SET{id_mot, #VAL{formation}|motsdf_id_mot{#ID_FORMATION}}
+ * 
+ * @api
+ * @param string objet
+ * @param int id_objet
+ *
+ * @return int
+ */
 function motsdf_id_mot($objet, $id_objet) {
 	$id_mot = sql_getfetsel('id_mot', 'spip_mots_liens', 'objet='.sql_quote($objet).' AND id_objet='.intval($id_objet));
 	
